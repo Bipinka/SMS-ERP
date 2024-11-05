e4011307d2aedf65e10db5757e03b0c5a2c81291
Rahul <mgrrahul200@gmail.com>
2024-11-05 10:54:27 +0545

    update

--- assets/javascript/bar.js
+++ assets/javascript/bar.js
@@ -1,22 +1,59 @@
 
 
-const ctx = document.getElementById('Bar');
+const ctx = document.getElementById("Bar");
 
-  const barChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['STUDENTS', 'TEACHERS', 'STAFFS'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 15],
-        borderWidth: 0.5
-      }]
+const barChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ['ATTANDENCE'],
+    datasets: [
+      {
+        label: "students",
+        data: [12],
+        borderWidth: 0.5,
+        borderRadius: 10,
+        backgroundColor: ["#64b5f6"],
+        // options: {
+        //   animation: {
+        //     onComplete: () => {
+        //       delayed = true;
+        //     },
+        //     delay: (context) => {
+        //       let delay = 1;
+        //       if (
+        //         context.type === "data" &&
+        //         context.mode === "default" &&
+        //         !delayed
+        //       ) {
+        //         delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        //       }
+        //       return delay;
+        //     },
+        //   },
+        // },
+      },
+       {
+        label:'teachers',
+         data:[16],
+        borderWidth:0.5,
+        borderRadius: 10,
+        backgroundColor:["#a4161a"],
+       },
+       {
+        label:"staffs",
+        data:[14],
+        borderWidth:0.5,
+        borderRadius: 10,
+        backgroundColor:["#788bff"],
+       }
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+  },
+  
+});
