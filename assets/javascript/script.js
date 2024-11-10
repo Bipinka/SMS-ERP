821a3052b390affa4c68acc27c0bd8f0ebb8ddef
Rahul <mgrrahul200@gmail.com>
2024-11-10 20:35:34 +0545

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,9 @@
-
-
 const arrow = document.querySelectorAll(".arrow");
 console.log(arrow);
-for ( var i = 0; i < arrow.length; i++)
-{
-  arrow[i].addEventListener("click", function(e)
-{
+for (let i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", function (e) {
     let dropDown = e.target.parentElement.parentElement;
     console.log(dropDown);
     dropDown.classList.toggle("showmenu");
-})
-}   
+  });
+}
